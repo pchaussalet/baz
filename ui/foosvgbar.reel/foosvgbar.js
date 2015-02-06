8f86aa8ef3548792d6ac6fa1c232e6ac8eeb3c94
Pierre Chaussalet <pchaussalet@gmail.com>
2015-02-06 00:22:45 +0000

    Add component foosvgbar

---
+++ ui/foosvgbar.reel/foosvgbar.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/foosvgbar.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Foosvgbar
+ * @extends Component
+ */
+exports.Foosvgbar = Component.specialize(/** @lends Foosvgbar# */ {
+    constructor: {
+        value: function Foosvgbar() {
+            this.super();
+        }
+    }
+});
