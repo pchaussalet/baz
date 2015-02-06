27a8aa25a863c81c1bb5c3b0ebdbaf133bf48ac2
Pierre Chaussalet <pchaussalet@gmail.com>
2015-02-06 00:25:30 +0000

    Add component foosvg

---
+++ ui/foosvg.reel/foosvg.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/foosvg.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Foosvg
+ * @extends Component
+ */
+exports.Foosvg = Component.specialize(/** @lends Foosvg# */ {
+    constructor: {
+        value: function Foosvg() {
+            this.super();
+        }
+    }
+});
