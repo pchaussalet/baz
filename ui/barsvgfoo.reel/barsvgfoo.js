d81f3d8435c5b634f7ece392374af3cfb7b56f12
Pierre Chaussalet <pchaussalet@gmail.com>
2015-02-06 00:24:49 +0000

    Add component barsvgfoo

---
+++ ui/barsvgfoo.reel/barsvgfoo.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/barsvgfoo.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Barsvgfoo
+ * @extends Component
+ */
+exports.Barsvgfoo = Component.specialize(/** @lends Barsvgfoo# */ {
+    constructor: {
+        value: function Barsvgfoo() {
+            this.super();
+        }
+    }
+});
